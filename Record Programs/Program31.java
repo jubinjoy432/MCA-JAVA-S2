b1044fbfeae958f7f3420c9f5a161bc84aac2be8
Jubin Joy <jubinjoy432@gmail.com>
2026-01-01 12:01:25 +0530

    Added till pgm35

---
+++ Record Programs/Program31.java
@@ -0,0 +1,42 @@
+class Printer {
+    synchronized void print(String documentName, int pages) {
+        System.out.println("Printing started: " + documentName);
+        for (int i = 1; i <= pages; i++) {
+            System.out.println(documentName + " - Printing page " + i);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+        }
+        System.out.println("Printing finished: " + documentName + "\n");
+    }
+}
+class PrinterThread extends Thread {
+    Printer printer;
+    String documentName;
+    int pages;
+
+    PrinterThread(Printer printer, String documentName, int pages) {
+        this.printer = printer;
+        this.documentName = documentName;
+        this.pages = pages;
+    }
+
+    public void run() {
+        printer.print(documentName, pages);
+    }
+}
+class Program31 {
+    public static void main(String args[]) {
+
+        Printer printer = new Printer();
+
+        PrinterThread job1 = new PrinterThread(printer, "Document1", 3);
+        PrinterThread job2 = new PrinterThread(printer, "Document2", 4);
+        PrinterThread job3 = new PrinterThread(printer, "Document3", 2);
+
+        job1.start();
+        job2.start();
+        job3.start();
+    }
+}
