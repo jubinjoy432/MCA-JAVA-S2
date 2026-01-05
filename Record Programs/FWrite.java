9526c23d91eef797c617e98e1b96df8b12482e21
jubinjoy432 <jubinjoy432@gmail.com>
2026-01-05 14:24:57 +0530

    Added File Program

---
+++ Record Programs/FWrite.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class FWrite{
+    public static void main(String args[])
+    {
+        FileOutputStream fout;
+        Scanner s;
+        s= new Scanner (System.in);
+        try{
+            System.out.println("Enetr the file name:");
+            String fn=s.nextLine();
+            fout=new FileOutputStream(fn);
+            while(true)
+               {
+                  System.out.println("Enter data/quit");
+                  String data=s.nextLine();
+                  if(data.equals("quit"))
+                    break;
+                  fout.write(data.getBytes());
+               }
+               fout.close();
+               }
+               catch(Exception e)
+               {
+                  System.out.println("Error"+e);
+               }
+
+        }
+    }
+
